2b65dce0a141d7f5fdbb790b45fe7e6dd60299e8
EAM26 <emuller2607@gmail.com>
2023-05-01 09:40:50 +0200

    Finished v1.0

--- src/main/java/com/example/techiteasymodel/contrlollers/TelevisionController.java
+++ src/main/java/com/example/techiteasymodel/contrlollers/TelevisionController.java
@@ -4,62 +4,71 @@ import com.example.techiteasymodel.exceptions.IllegalNameLengthException;
 import com.example.techiteasymodel.exceptions.RecordNotFoundException;
 import com.example.techiteasymodel.models.Television;
 import com.example.techiteasymodel.repositories.TelevisionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/televisions")
 public class TelevisionController {
 
+    private final TelevisionRepository repos;
 
-    @Autowired
-    TelevisionRepository repos;
+    public TelevisionController(TelevisionRepository repos) {
+        this.repos = repos;
+    }
+
 
     @GetMapping
     public ResponseEntity<Iterable<Television>> getTelevisions() {
         return new ResponseEntity(repos.findAll(), HttpStatus.OK);
     }
 
+
     @GetMapping("/{id}")
-    public ResponseEntity<Television> getTelevision(@PathVariable int id) throws RecordNotFoundException {
-        Optional<Television> tv = repos.findById((long) id);
-        if(tv!=null) {
-            return new ResponseEntity(tv, HttpStatus.OK);
+    public ResponseEntity<Television> getTelevision(@PathVariable Long id) throws RecordNotFoundException {
+        Optional<Television> optionalTelevision = repos.findById(id);
+        if(optionalTelevision.isEmpty()) {
+            throw new RecordNotFoundException("No television found with id: " + id);
         }
-        else {
-            throw new RecordNotFoundException("Not a legal index number");
-        }
+        return new ResponseEntity(optionalTelevision.get(), HttpStatus.OK);
     }
+
 
     @PostMapping
     public ResponseEntity<Television> addTelevision(@RequestBody Television tv) throws IllegalNameLengthException {
         if (tv.getName().length() > 20) {
-            throw new IllegalNameLengthException("Name must not be longer than 20 chars.");
+            throw new IllegalNameLengthException("Name cannot be longer than 20 chars.");
         }
         repos.save(tv);
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentRequest().path("/" + tv.getId()).toUriString());
         return ResponseEntity.created(uri).body(tv);
     }
 
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Object> deleteTelevision(@PathVariable int id) {
-        repos.deleteById((long) id);
+    public ResponseEntity<Object> deleteTelevision(@PathVariable Long id) {
+        repos.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
-//    @PutMapping("/{id}")
-//    public ResponseEntity<Television> updateTelevision(@RequestBody Television tv, @PathVariable int id) {
-//        Optional<Television> oldTelevision = repos.findById((long) id);
-//        if(!oldTelevision.isEmpty()) {
-//            oldTelevision.set
-//        }
-//    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateTelevision(@RequestBody Television tv, @PathVariable Long id) {
+        Optional<Television> optionalTelevision = repos.findById(id);
+        if(optionalTelevision.isEmpty()) {
+            throw new RecordNotFoundException("No television found with id: " + id);
+        }
+        Television television = optionalTelevision.get();
+        television.setBrand(tv.getBrand());
+        television.setType(tv.getType());
+        television.setName(tv.getName());
+        television.setPrice(tv.getPrice());
+        repos.save(television);
+        return new ResponseEntity<>((television.getName() + " added to inventory"), HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/techiteasymodel/models/Television.java
+++ src/main/java/com/example/techiteasymodel/models/Television.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Getter
+@Setter
 @Entity
 @Table(name="televisions")
 public class Television {
@@ -18,18 +22,18 @@ public class Television {
     private String brand;
     private String name;
     private Double price;
-//    private Double refreshRate;
-//    private Double availableSize;
-//    private String screenType;
-//    private String screenQuality;
-//    private Boolean smartTv;
-//    private Boolean wifi;
-//    private Boolean voiceControl;
-//    private Boolean hdr;
-//    private Boolean bluetooth;
-//    private Boolean ambiLight;
-//    private Integer originalStock;
-//    private Integer sold;
+    private Double refreshRate;
+    private Double availableSize;
+    private String screenType;
+    private String screenQuality;
+    private Boolean smartTv;
+    private Boolean wifi;
+    private Boolean voiceControl;
+    private Boolean hdr;
+    private Boolean bluetooth;
+    private Boolean ambiLight;
+    private Integer originalStock;
+    private Integer sold;
 
 
     public Television() {
@@ -41,25 +45,5 @@ public class Television {
         this.brand = brand;
         this.name = name;
         this.price = price;
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Double getPrice() {
-        return price;
     }
 }
